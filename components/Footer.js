d541c94062bd05923b157130f8dc67fd2411d6a4
Brian Chapman <chapman.brian86@gmail.com>
2023-07-11 00:55:11 -0400

    Created basic footer component

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import styles from '../styles/components/Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>All RIghts Reserved. Copyright @ 2023</p>
+        </footer>
+    );
+   };
+export default Footer;
